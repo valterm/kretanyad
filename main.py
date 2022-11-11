47f6484e56e0f80787c8dbb11f0928a28ffe7557
BigFudge <valter.martin@hotmail.com>
2022-11-11 04:15:54 +0100

    add bot

---
+++ main.py
@@ -0,0 +1,61 @@
+from telegram import Update
+from telegram.ext import Updater,CallbackContext,CommandHandler
+import argparse
+import json
+from random import randint
+import logging
+
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+def get_random_insult() -> str:
+    f = open('./dirty.json', 'r')
+    json_data = json.loads(f.read())
+    max_n = len(json_data['DirtyWords'])
+    n = randint(0, max_n-1)
+
+    return(json_data['DirtyWords'][n].lower())
+
+
+def start(update: Update, context: CallbackContext):
+    user = update.effective_user.username
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"@{user}, vigyázz, mert csúnyán beszélek!")
+
+def helper(update: Update, context: CallbackContext):
+    user = update.effective_user.username
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"@{user}, ha nem tudod kitalálni mire jó ez a bot, nem tudok rajtad segíteni.")
+
+def insult(update: Update, context: CallbackContext):
+    insult = get_random_insult()
+    user = update.effective_user.username
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=f"@{user}, te {insult}.")
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument('token')
+    args = parser.parse_args()
+
+    token = args.token 
+
+    updater = Updater(token=token)
+    
+    dispatcher = updater.dispatcher
+
+    start_handler = CommandHandler('start', start)
+    help_handler = CommandHandler('help', helper)
+    insult_handler = CommandHandler('insult',insult)
+    dispatcher.add_handler(start_handler)
+    dispatcher.add_handler(help_handler)
+    dispatcher.add_handler(insult_handler)
+
+    updater.start_polling()
+
+
+if __name__ == "__main__":
+    main()
+
+
